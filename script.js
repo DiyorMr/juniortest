709400f9e1c16571ef91461902a8e4c73b311aa6
DiyorMr <dmuxiddinov594@gmail.com>
2026-06-19 10:02:51 +0500

    tests

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-let car = ["Bmw", "Kia", "Audi"];
-console.log(car.includes('Kia'));
+// var name = 'Ali'
+// console.log(name);
+
+// var name = 'Vali'
+// console.log(name)
+
+// let age = 12
+// console.log(age)
+
+// const pi = 3.14
+// console.log(pi)
+
+// var name = "Alisher";
+// console.log(name);
+
+// var number = 64865533;
+// console.log(number);
+
+// var male = true;
+// console.log(male);
+
+// let i = 17;
+
+// if (i >= 16) {
+//   console.log("Passport olish mumkin");
+// } else {
+//   console.log("Hali baby");
+// }
+
+let user = {
+  name: "Ali",
+  age: 15,
+  male: true,
+};
+console.log(user);
+
+let user1 = ["Ali", "Vali", "Sherka", "Messi"];
+user1.push("Shoxruz");
+console.log(user1);
