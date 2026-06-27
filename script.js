2d76a0b1021e842b7edde9b48c777720de6a3215
DiyorMr <dmuxiddinov594@gmail.com>
2026-06-27 11:48:24 +0500

    done web

--- script.js
+++ script.js
@@ -1,39 +1,11 @@
-// var name = 'Ali'
-// console.log(name);
+var name = "Aziz";
+console.log(name);
 
-// var name = 'Vali'
-// console.log(name)
+var age = 15;
+console.log(age);
 
-// let age = 12
-// console.log(age)
+var surname = "Aliev";
+console.log(surname);
 
-// const pi = 3.14
-// console.log(pi)
-
-// var name = "Alisher";
-// console.log(name);
-
-// var number = 64865533;
-// console.log(number);
-
-// var male = true;
-// console.log(male);
-
-// let i = 17;
-
-// if (i >= 16) {
-//   console.log("Passport olish mumkin");
-// } else {
-//   console.log("Hali baby");
-// }
-
-let user = {
-  name: "Ali",
-  age: 15,
-  male: true,
-};
-console.log(user);
-
-let user1 = ["Ali", "Vali", "Sherka", "Messi"];
-user1.push("Shoxruz");
-console.log(user1);
+var car = "BMW";
+console.log(car);
