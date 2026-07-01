63245ed14e6f7ac9178802f81428f1c252a7446d
DiyorMr <dmuxiddinov594@gmail.com>
2026-07-01 19:12:14 +0500

    fixxeeeddd

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-var name = "Ali";
-console.log(name);
+let a;
+let y = null;
 
-var name = "Vali";
-console.log(name);
-
-var name = "Aziz";
-console.log(name);
-
-let city = "London";
-console.log(city);
-
-let city = "Paris";
-console.log(city);
-
-const pi = 3.14;
-console.log(pi);
+console.log(typeof a);
+console.log(typeof y);
